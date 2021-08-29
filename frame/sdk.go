5b40e45ca5d3ef2f1711c4feba0c0e92bf7108a4
Unixeno <li1397937810@gmail.com>
2021-08-29 22:49:56 +0800

    fix sdk name
    Change-Id: Ic4af7ed0ef6b5e35402bb6823ee8e1998c6af0ca

--- frame/sdk.go
+++ frame/sdk.go
@@ -2,8 +2,8 @@ package frame
 
 import "fmt"
 
-const SDKPackage = `"gootprint/sdk"` // trace sdk used in import statement
-const SDKPackagePrefix = "sdk."      // the prefix to access trace sdk package
+const SDKPackage = `"github.com/Unixeno/gootprint/sdk"` // trace sdk used in import statement
+const SDKPackagePrefix = "sdk."                         // the prefix to access trace sdk package
 
 func genLineCode(method, args string) string {
 	return fmt.Sprintf("%s%s(%s);", SDKPackagePrefix, method, args)
