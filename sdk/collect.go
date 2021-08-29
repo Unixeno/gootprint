377091b77f18b312c18e425dd571c2ae7e6aed0c
Unixeno <li1397937810@gmail.com>
2021-08-29 22:55:58 +0800

    count the number of collect
    Change-Id: I07c46b2f1363023156495470443cd7e1a4bcbd3f

--- sdk/collect.go
+++ sdk/collect.go
@@ -1,7 +1,21 @@
 package sdk
 
-import "fmt"
+import (
+	"fmt"
+	"sync/atomic"
+)
+
+var counter uint64
 
 func Collect(info string) {
+	atomic.AddUint64(&counter, 1)
 	fmt.Println(info)
+}
+
+func ResetCounter() {
+	atomic.StoreUint64(&counter, 0)
+}
+
+func GetCounter() uint64 {
+	return atomic.LoadUint64(&counter)
 }
